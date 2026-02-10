6b2b7340dca15a36ffa63a2b2e7ea66d4785ae41
RichardRadulescu <Richard.Radulescu@cognizant.com>
2026-02-10 12:28:31 +0200

    added custom hook for watched movies and integrated with local storage

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,20 +1,20 @@
 import { useEffect, useState } from "react"
 import "../styles/movie-card.css"
 
-export function MovieCard({movie}){
-    const [watched, setWatched]= useState(false)
+export function MovieCard({movie, toggleWatched, isWatched}){
      
-    //useEffect() -> get local storage
-    const handleWatched= (prev)=>{ setWatched(!watched)
-        //set local storage
-    }
     return (
         <div className="movie-card">
             <h1>{movie.title}</h1>
             <p>Category: {movie.genre}</p>
             <p>Score: {movie.rating}</p>
             <img src={`/movies/images/${movie.image}`} alt={movie.title} />
-            <button onClick={handleWatched}>Add Watchlist</button>
+
+            <button onClick={()=> toggleWatched(movie)}
+                className={isWatched(movie)? 'active': null }
+            >
+                {isWatched(movie)? "Remove from Watchlist" : "Add Watchlist"}
+                </button>
         </div>
     )
 }
