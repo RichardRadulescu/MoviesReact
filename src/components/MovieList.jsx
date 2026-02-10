113c96b21ac09bc08a37a670e100668869c8e0d8
RichardRadulescu <Richard.Radulescu@cognizant.com>
2026-02-10 09:56:13 +0200

    Added filtering and sorting to movie list

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,6 +4,18 @@ import "../styles/movie-list.css"
 
 export function MovieList(){
     const [movies, setMovies]= useState([])
+    const [query, setQuery]= useState('')
+    const [isSortedAlphabetical, setIsSortedAlphabetical] = useState(false)
+    const [isSortedRating, setIsSortedRating] = useState(false)
+
+
+    let mutatedMovies= movies.filter((movie)=> movie.title.toLowerCase().startsWith(query.toLowerCase()))
+    
+    if (isSortedAlphabetical)
+        mutatedMovies= [...mutatedMovies].sort((m1,m2)=> m1.title.localeCompare(m2.title))
+
+    if (isSortedRating)
+        mutatedMovies= [...mutatedMovies].sort((m1,m2)=> m1.rating - m2.rating)
 
     useEffect(()=>{
         async function getMovies() {
@@ -16,7 +28,29 @@ export function MovieList(){
 
     return (
         <div className="movies-grid">
-            {movies.map((m)=>{
+            <div className="movies-controls">
+                <form onSubmit={(e)=>{e.preventDefault()}}>
+                    <label htmlFor="search-movie"></label>
+                    <input type="text" name="search-movie" placeholder="..."
+                        onChange={e => setQuery(e.target.value)}></input>
+                    <button type="submit">Search</button>
+                </form>
+                <div className="sort-buttons">
+                    <button type="button"
+                        className={isSortedAlphabetical ? "active" : ""}
+                    
+                        onClick={()=>{setIsSortedAlphabetical(prev=> !prev)
+                        setIsSortedRating(false)
+                    }}>Sort A-Z</button>
+                    <button type="button"
+                        className={isSortedRating ? "active" : ""} 
+                    
+                        onClick={()=>{setIsSortedRating(prev=> !prev)
+                        setIsSortedAlphabetical(false)
+                    }}>Sort rating</button>
+                </div>
+            </div>
+            {mutatedMovies.map((m)=>{
                return <MovieCard movie={m} key={m.id}/> 
             })}
         </div>
